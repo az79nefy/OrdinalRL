45a5c8a5bd3043db41bbdf012e7f6843370b17a6
az79nefy <alexander.zap@stud.tu-darmstadt.de>
2018-11-01 15:48:15 +0100

    Added versions of sarsa/ordinal-sarsa for continuous observation space
    - Re-implemented sarsa and ordinal-sarsa for the CartPole-problem by discretizing continuous observation space

---
+++ sarsa/gym_ordinal_sarsa_discretized.py
@@ -0,0 +1,187 @@
+import gym
+import numpy as np
+import random
+
+
+'''  CONFIGURATION  '''
+
+env = gym.make('CartPole-v0')
+
+# Learning rate
+alpha = 0.1
+# Discount factor
+gamma = 0.9
+# Epsilon in epsilon-greedy exploration (for action choice)
+epsilon = 1.0
+
+# Number of episodes to be run
+n_episodes = 50000
+# Maximal timesteps to be used per episode
+max_timesteps = 1000
+
+# Flag whether to randomize action estimates at initialization
+randomize = False
+# Number of ordinals (possible different rewards)
+n_ordinals = 2
+
+''' INITIALIZATION '''
+
+# number of possible actions
+n_actions = env.action_space.n
+
+# discretize the observation space
+pole_theta_space = np.linspace(-0.20943951, 0.20943951, 10)
+pole_theta_vel_space = np.linspace(-4, 4, 10)
+cart_pos_space = np.linspace(-2.4, 2.4, 10)
+cart_vel_space = np.linspace(-4, 4, 10)
+
+borda_values = {}
+for i in range(len(cart_pos_space) + 1):
+    for j in range(len(cart_vel_space) + 1):
+        for k in range(len(pole_theta_space) + 1):
+            for l in range(len(pole_theta_vel_space) + 1):
+                for a in range(n_actions):
+                    if randomize:
+                        borda_values[(i, j, k, l), a] = random.random() / 10
+                    else:
+                        borda_values[(i, j, k, l), a] = 1.0
+
+ordinal_values = {}
+for i in range(len(cart_pos_space) + 1):
+    for j in range(len(cart_vel_space) + 1):
+        for k in range(len(pole_theta_space) + 1):
+            for l in range(len(pole_theta_vel_space) + 1):
+                for a in range(n_actions):
+                    ordinal_values[(i, j, k, l), a] = [0.0 for x in range(n_ordinals)]
+
+
+'''  FUNCTION DEFINITION  '''
+
+
+def get_discrete_observation(obs):
+    cart_x, cart_x_dot, cart_theta, cart_theta_dot = obs
+    cart_x = int(np.digitize(cart_x, cart_pos_space))
+    cart_x_dot = int(np.digitize(cart_x_dot, cart_vel_space))
+    cart_theta = int(np.digitize(cart_theta, pole_theta_space))
+    cart_theta_dot = int(np.digitize(cart_theta_dot, pole_theta_vel_space))
+
+    return cart_x, cart_x_dot, cart_theta, cart_theta_dot
+
+
+# Mapping of reward value to ordinal reward (has to be configured per game)
+def reward_to_ordinal(reward_value):
+    if done & (episode_reward != 200):
+        return 0
+    else:
+        return int(reward_value)
+
+
+# Updates Q_Values based on probability of ordinal reward occurrence for each action
+def update_ordinal_values(prev_obs, prev_act, obs, act, ordinal):
+    for i in range(n_ordinals):
+        ordinal_values[prev_obs, prev_act][i] *= (1 - alpha)
+        ordinal_values[prev_obs, prev_act][i] += alpha * (gamma * ordinal_values[obs, act][i])
+
+    ordinal_values[prev_obs, prev_act][ordinal] += alpha
+
+
+# Updates borda_values for one observation given the ordinal_values
+def update_borda_scores():
+    # sum up all ordinal values per action for given observation
+    ordinal_value_sum_per_action = np.zeros(n_actions)
+    for action_a in range(n_actions):
+        for ordinal_value in ordinal_values[prev_observation, action_a]:
+            ordinal_value_sum_per_action[action_a] += ordinal_value
+
+    # count actions whose ordinal value sum is not zero (no comparision possible for actions without ordinal_value)
+    non_zero_action_count = np.count_nonzero(ordinal_value_sum_per_action)
+    actions_to_compare_count = non_zero_action_count - 1
+
+    # compute borda_values for action_a (probability that action_a wins against any other action)
+    for action_a in range(n_actions):
+        # if action has not yet recorded any ordinal values, action has to be played (set borda_value to 1.0)
+        if ordinal_value_sum_per_action[action_a] == 0:
+            borda_values[prev_observation, action_a] = 1.0
+            continue
+
+        if actions_to_compare_count < 1:
+            # set lower than 1.0 (borda_value for zero_actions is 1.0)
+            borda_values[prev_observation, action_a] = 0.5
+        else:
+            # over all actions: sum up the probabilities that action_a wins against the given action
+            winning_probability_a_sum = 0
+            # compare action_a to all other actions
+            for action_b in range(n_actions):
+                if action_a == action_b:
+                    continue
+                # not comparable if action_b has no ordinal_values
+                if ordinal_value_sum_per_action[action_b] == 0:
+                    continue
+                else:
+                    # probability that action_a wins against action_b
+                    winning_probability_a = 0
+                    # running ordinal probability that action_b is worse than current investigated ordinal
+                    worse_probability_b = 0
+                    for ordinal_count in range(n_ordinals):
+                        ordinal_probability_a = ordinal_values[prev_observation, action_a][ordinal_count] \
+                                                     / ordinal_value_sum_per_action[action_a]
+                        # ordinal_probability_b is also the tie probability
+                        ordinal_probability_b = (ordinal_values[prev_observation, action_b][ordinal_count] /
+                                                 ordinal_value_sum_per_action[action_b])
+                        winning_probability_a += ordinal_probability_a * \
+                            (worse_probability_b + ordinal_probability_b / 2.0)
+                        worse_probability_b += ordinal_probability_b
+                    winning_probability_a_sum += winning_probability_a
+            # normalize summed up probabilities with number of actions that have been compared
+            borda_values[prev_observation, action_a] = winning_probability_a_sum / actions_to_compare_count
+
+
+# Chooses action with epsilon greedy exploration policy
+def choose_action(obs):
+    possible_actions = np.array([borda_values[obs, act] for act in range(n_actions)])
+    greedy_action = np.argmax(possible_actions)
+    # choose random action with probability epsilon
+    if np.random.random() < epsilon:
+        return random.randrange(n_actions)
+    # greedy action is chosen with probability (1 - epsilon)
+    else:
+        return greedy_action
+
+
+''' EXECUTION '''
+
+episode_rewards = []
+for i_episode in range(n_episodes):
+    observation = get_discrete_observation(env.reset())
+    action = choose_action(observation)
+
+    prev_observation = None
+    prev_action = None
+
+    episode_reward = 0
+    for t in range(max_timesteps):
+        observation_cont, reward, done, info = env.step(action)
+        observation = get_discrete_observation(observation_cont)
+        # next action to be executed (based on new observation)
+        action = choose_action(observation)
+        episode_reward += reward
+
+        if prev_observation is not None:
+            received_ordinal = reward_to_ordinal(reward)
+            # update ordinal_values with received ordinal
+            update_ordinal_values(prev_observation, prev_action, observation, action, received_ordinal)
+            # update borda_values with updated ordinal_values
+            update_borda_scores()
+
+        prev_observation = observation
+        prev_action = action
+
+        if done:
+            epsilon -= 2 / n_episodes if epsilon > 0 else 0
+            episode_rewards.append(episode_reward)
+            if i_episode % 100 == 99:
+                print("Episode {} finished. Average reward since last check: {}".format(i_episode + 1, np.mean(episode_rewards)))
+                episode_rewards = []
+            break
+
+env.close()
---
+++ sarsa/gym_sarsa_discretized.py
@@ -0,0 +1,114 @@
+import gym
+import numpy as np
+import random
+
+
+'''  CONFIGURATION  '''
+
+env = gym.make('CartPole-v0')
+
+# Learning rate
+alpha = 0.1
+# Discount factor
+gamma = 0.9
+# Epsilon in epsilon-greedy exploration (for action choice)
+epsilon = 1.0
+
+# Number of episodes to be run
+n_episodes = 50000
+# Maximal timesteps to be used per episode
+max_timesteps = 1000
+
+# Flag whether to randomize action estimates at initialization
+randomize = True
+
+
+''' INITIALIZATION '''
+
+# number of possible actions
+n_actions = env.action_space.n
+
+# discretize the observation space
+pole_theta_space = np.linspace(-0.20943951, 0.20943951, 10)
+pole_theta_vel_space = np.linspace(-4, 4, 10)
+cart_pos_space = np.linspace(-2.4, 2.4, 10)
+cart_vel_space = np.linspace(-4, 4, 10)
+
+# Q_Values (n-dimensional array with float-value for each action in each (n-1)-dimensional observation space)
+q_values = {}
+for i in range(len(cart_pos_space) + 1):
+    for j in range(len(cart_vel_space) + 1):
+        for k in range(len(pole_theta_space) + 1):
+            for l in range(len(pole_theta_vel_space) + 1):
+                for a in range(n_actions):
+                    if randomize:
+                        q_values[(i, j, k, l), a] = random.random() / 10
+                    else:
+                        q_values[(i, j, k, l), a] = 0
+
+
+'''  FUNCTION DEFINITION  '''
+
+
+def get_discrete_observation(obs):
+    cart_x, cart_x_dot, cart_theta, cart_theta_dot = obs
+    cart_x = int(np.digitize(cart_x, cart_pos_space))
+    cart_x_dot = int(np.digitize(cart_x_dot, cart_vel_space))
+    cart_theta = int(np.digitize(cart_theta, pole_theta_space))
+    cart_theta_dot = int(np.digitize(cart_theta_dot, pole_theta_vel_space))
+
+    return cart_x, cart_x_dot, cart_theta, cart_theta_dot
+
+
+# Updates Q_Values based on probability of ordinal reward occurrence for each action
+def update_q_values(prev_obs, prev_act, obs, act, rew):
+    q_old = q_values[prev_obs, prev_act]
+    q_new = (1 - alpha) * q_old + alpha * (rew + gamma * q_values[obs, act])
+    q_values[prev_obs, prev_act] = q_new
+
+
+# Chooses action with epsilon greedy exploration policy
+def choose_action(obs):
+    possible_actions = np.array([q_values[obs, act] for act in range(n_actions)])
+    greedy_action = np.argmax(possible_actions)
+    # choose random action with probability epsilon
+    if random.random() < epsilon:
+        return random.randrange(n_actions)
+    # greedy action is chosen with probability (1 - epsilon)
+    else:
+        return greedy_action
+
+
+''' EXECUTION '''
+
+episode_rewards = []
+for i_episode in range(n_episodes):
+    observation = get_discrete_observation(env.reset())
+    action = choose_action(observation)
+
+    prev_observation = None
+    prev_action = None
+
+    episode_reward = 0
+    for t in range(max_timesteps):
+        observation_cont, reward, done, info = env.step(action)
+        observation = get_discrete_observation(observation_cont)
+        # next action to be executed (based on new observation)
+        action = choose_action(observation)
+        episode_reward += reward
+
+        if prev_observation is not None:
+            update_q_values(prev_observation, prev_action, observation, action, episode_reward)
+
+        prev_observation = observation
+        prev_action = action
+
+        if done:
+            epsilon -= 2 / n_episodes if epsilon > 0 else 0
+            episode_rewards.append(episode_reward)
+            if i_episode % 100 == 99:
+                print("Episode {} finished. Average reward since last check: {}".format(i_episode + 1, np.mean(episode_rewards)))
+                episode_rewards = []
+            break
+
+env.close()
