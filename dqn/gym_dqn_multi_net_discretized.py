7dc433f7acb1c85a0e725f1cc53dcb57afdd4efa
az79nefy <alexander.zap@stud.tu-darmstadt.de>
2018-11-23 13:54:20 +0100

    Added discretization for DQN algorithms in continuous observation space

--- dqn/gym_dqn_multi_net_discretized.py
+++ dqn/gym_dqn_multi_net_discretized.py
@@ -6,6 +6,7 @@ from keras.layers import Dense
 from keras.optimizers import Adam
 import matplotlib.pyplot as plt
 import random
+import itertools
 
 
 '''  CONFIGURATION  '''
@@ -24,6 +25,14 @@ n_episodes = 200
 # Maximal timesteps to be used per episode
 max_timesteps = 1000
 
+# Discretize the observation space (specify manually)
+n_observations = 11**4
+cart_pos_space = np.linspace(-2.4, 2.4, 10)
+cart_vel_space = np.linspace(-4, 4, 10)
+pole_theta_space = np.linspace(-0.20943951, 0.20943951, 10)
+pole_theta_vel_space = np.linspace(-4, 4, 10)
+observation_space = [cart_pos_space, cart_vel_space, pole_theta_space, pole_theta_vel_space]
+
 
 ''' INITIALIZATION '''
 
@@ -35,6 +44,16 @@ n_inputs = env.observation_space.shape[0]
 batch_size = 64
 memory = deque(maxlen=20000)
 
+# List of all possible discrete observations
+observation_range = [range(len(i)+1) for i in observation_space]
+
+# Dictionary that maps discretized observations to array indices
+observation_to_index = {}
+index_counter = 0
+for observation in list(itertools.product(*observation_range)):
+    observation_to_index[observation] = index_counter
+    index_counter += 1
+
 
 '''  FUNCTION DEFINITION  '''
 
@@ -44,7 +63,7 @@ def build_model():
     neural_net = Sequential()
     neural_net.add(Dense(24, input_dim=n_inputs, activation='relu'))
     neural_net.add(Dense(24, activation='relu'))
-    neural_net.add(Dense(n_actions, activation='linear'))
+    neural_net.add(Dense(1, activation='linear'))
     neural_net.compile(loss='mse', optimizer=Adam(lr=alpha))
     return neural_net
 
@@ -70,23 +89,31 @@ def remember(prev_obs, prev_act, obs, rew, d):
 
 def replay(batch_size):
     mini_batch = random.sample(memory, batch_size)
-    x_batch, y_batch = [], []
     for prev_obs, prev_act, obs, rew, d in mini_batch:
-        prediction = model.predict(prev_obs)
         if not d:
-            target = rew + gamma * np.max(model.predict(obs)[0])
+            action_predictions = []
+            for act_net in action_nets:
+                action_predictions.append(act_net.predict(obs)[0])
+            target = rew + gamma * np.max(action_predictions)
         else:
             target = rew
         # fit predicted value of previous action in previous observation to target value of max_action
-        prediction[0][prev_act] = target
-        x_batch.append(prev_obs[0])
-        y_batch.append(prediction[0])
-    model.fit(np.array(x_batch), np.array(y_batch), batch_size=len(x_batch), verbose=0)
+        action_nets[prev_act].fit(prev_obs, [[target]], verbose=0)
+
+
+def get_discrete_observation(obs):
+    discrete_observation = []
+    for obs_idx in range(len(obs)):
+        discrete_observation.append(int(np.digitize(obs[obs_idx], observation_space[obs_idx])))
+    return tuple(discrete_observation)
 
 
 # Chooses action with epsilon greedy exploration policy
 def choose_action(obs):
-    greedy_action = np.argmax(model.predict(obs)[0])
+    action_predictions = []
+    for act_net in action_nets:
+        action_predictions.append(act_net.predict(obs)[0])
+    greedy_action = np.argmax(action_predictions)
     # choose random action with probability epsilon
     if random.random() < epsilon:
         return random.randrange(n_actions)
@@ -103,10 +130,14 @@ win_rate_list = []
 episode_reward_list = []
 mean_reward_list = []
 
-model = build_model()
+# creation of a neural net for every action
+action_nets = []
+for act in range(n_actions):
+    action_nets.append(build_model())
+
 for i_episode in range(n_episodes):
-    observation_cont = env.reset()
-    observation = np.reshape(observation_cont, [1, n_inputs])
+    observation = get_discrete_observation(env.reset())
+    observation = np.reshape(observation, [1, n_inputs])
     action = choose_action(observation)
 
     prev_observation = None
@@ -115,7 +146,8 @@ for i_episode in range(n_episodes):
     episode_reward = 0
     for t in range(max_timesteps):
         observation_cont, reward, done, info = env.step(action)
-        observation = np.reshape(observation_cont, [1, n_inputs])
+        observation = get_discrete_observation(observation_cont)
+        observation = np.reshape(observation, [1, n_inputs])
         # next action to be executed (based on new observation)
         action = choose_action(observation)
         episode_reward += reward
